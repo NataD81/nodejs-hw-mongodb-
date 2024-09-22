869b1767497ec2d7943ec61be274c69856bd20cd
NataD81 <dvoryanchuk81@gmail.com>
2024-09-22 12:46:06 +0300

    update hw3-crud

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,11 +1,19 @@
-import { Schema, model } from "mongoose";
+import { Schema, model } from 'mongoose';
 
-const contactSchema = new Schema({
+const contactSchema = new Schema(
+  {
     name: { type: String, required: true },
     phoneNumber: { type: String, required: true },
     email: { type: String },
     isFavourite: { type: Boolean, default: false },
-    contactType: { type: String, enum: ['work', 'home', 'personal'], required: true, default: 'personal' },
-  }, { timestamps: true });
+    contactType: {
+      type: String,
+      enum: ['work', 'home', 'personal'],
+      required: true,
+      default: 'personal',
+    },
+  },
+  { timestamps: true },
+);
 
-  export const Contact = model('contacts', contactSchema);
+export const ContactsCollection = model('contacts', contactSchema);
