677d78d69bfcd5a58fa8b5c8ce02509cb6144a86
NataD81 <dvoryanchuk81@gmail.com>
2024-10-16 21:12:15 +0300

    hw6 -2

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -2,14 +2,16 @@ import nodemailer from 'nodemailer';
 import { SMTP } from '../constants/index.js';
 import { env } from '../utils/env.js';
 
-export const sendMail = nodemailer.createTransport({
+const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
-  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
 });
 
+export const sendMail = async (options) => {
+  return await transporter.sendMail(options);
+};
 
