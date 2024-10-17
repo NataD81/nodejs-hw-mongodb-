397b07ba0992aeab3bc940d00e3ab3feccbbafa5
NataD81 <dvoryanchuk81@gmail.com>
2024-10-17 21:56:28 +0300

    hw6-2

--- src/constants/index.js
+++ src/constants/index.js
@@ -4,6 +4,10 @@ export const SORT_ORDER = {
   ASC: 'asc',
   DESC: 'desc',
 };
+
+export const FIFTEEN_MINUTES = 15 * 60 * 1000; 
+export const THIRTY_DAYS = 30 * 24 * 60 * 60 * 1000;
+
 export const SMTP = {
   SMTP_HOST: 'SMTP_HOST',
   SMTP_PORT: 'SMTP_PORT',
