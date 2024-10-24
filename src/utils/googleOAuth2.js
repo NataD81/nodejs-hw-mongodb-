5396e78ed0e7b478d4500ea0d1eaaf941c8dc419
NataD81 <dvoryanchuk81@gmail.com>
2024-10-24 10:44:05 +0300

    hw7 up

--- src/utils/googleOAuth2.js
+++ src/utils/googleOAuth2.js
@@ -4,14 +4,15 @@ import { readFile } from 'fs/promises';
 import { env } from './env.js';
 import createHttpError from 'http-errors';
 
-const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
+// const PATH_JSON = path.join(process.cwd(), 'google-oauth.json');
 
-const oauthConfig = JSON.parse(await readFile(PATH_JSON));
+// const oauthConfig = JSON.parse(await readFile(PATH_JSON));
 
 const googleOAuthClient = new OAuth2Client({
   clientId: env('GOOGLE_AUTH_CLIENT_ID'),
   clientSecret: env('GOOGLE_AUTH_CLIENT_SECRET'),
-  redirectUri: oauthConfig.web.redirect_uris[0],
+  redirectUri: env('GOOGLE_AUTH_REDIRECT_URI')
+  // redirectUri: oauthConfig.web.redirect_uris[0],
 });
 
 export const generateAuthUrl = () =>
